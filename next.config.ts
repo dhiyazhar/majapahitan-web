42b99ba8953ebc9ec11efa9b913c313f12582228
Azhar Dhiyaulhaq <dhiyazhar@gmail.com>
2026-09-06 15:16:02 +0700

    fix(deploy): include sharp linux binaries in serverless output tracing

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,13 @@ import { withPayload } from "@payloadcms/next/withPayload";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: ['sharp'],
+  outputFileTracingIncludes: {
+    '**/*': [
+      './node_modules/@img/sharp-libvips-linux-x64/**/*',
+      './node_modules/@img/sharp-linux-x64/**/*',
+    ],
+  },
 };
 
 export default withPayload(nextConfig);
